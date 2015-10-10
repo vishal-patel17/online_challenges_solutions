6bb0f48cf9c41837a49eb3d9cf7ed07436a1169b
vishal-patel17 <vishal92.p@gmail.com>
2015-10-10 23:05:32 +0000

    third commit

--- factorial_challenge.cpp
+++ factorial_challenge.cpp
@@ -1,4 +1,26 @@
+/* Input
 
+An integer t, 1<=t<=100, denoting the number of testcases, followed by t lines, each containing a single integer n, 1<=n<=100.
+Output
+
+For each integer n given at input, display a line with the value of n!
+Example
+Sample input:
+
+4
+1
+2
+5
+3
+
+Sample output:
+
+1
+2
+120
+6
+
+  */
 #include <iostream>
 #include <forward_list>
 using namespace std;
